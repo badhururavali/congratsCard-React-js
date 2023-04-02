31be3937c2f0a209bfef38413b69f39cf5726a8f
ravali <badhururavali@gmail.com>
2023-04-02 05:49:00 +0000

    react js coding practice 2

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="bgImage">
+    <h1 className="heading">Congratulations</h1>
+    <div className="subContainer">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <p className="paragraph">Kiran V</p>
+      <p className="about">
+        Vishnu Institute of Computer Education and Technology,Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
